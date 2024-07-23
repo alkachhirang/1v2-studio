20c742c36a282fc6d8ee90644cb8b35d0b51c4ac
alkachhirang <alkachhirang3@gmail.com>
2024-07-23 10:34:57 +0530

    add common-folder

---
+++ src/components/Common/Helper.tsx
@@ -0,0 +1,39 @@
+//-----------navbar-----------
+export const navData = [
+    {
+      Link: "Home",
+      Path: "#Home",
+    },
+    {
+      Link: "Project",
+      Path: "#Project",
+    },
+    {
+      Link: "Vision",
+      Path: "#Vision",
+    },
+    {
+      Link: "Game-Fi",
+      Path: "#GameFi",
+    },
+    {
+      Link: "Sport2Earn",
+      Path: "#Sport2Earn",
+    },
+    {
+      Link: "Mint RoadMap",
+      Path: "MintRoadMap",
+    },
+    {
+      Link: "Utility",
+      Path: "#Utility",
+    },
+    {
+      Link: "Team",
+      Path: "#Team",
+    },
+    {
+      Link: "FAQs",
+      Path: "#FAQs",
+    },
+  ];
